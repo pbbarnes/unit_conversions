3195c498009555e067fe19c0faa20bec86d2447a
pbbarnes <paulbbarnes91@gmail.com>
2015-10-14 17:43:04 -1000

    Create convert.py

---
+++ src/convert.py
@@ -0,0 +1,11 @@
+def kilometers_to_miles(value):
+    return value/1.609
+
+def miles_to_kilometers(value):
+    return value * 1.609
+
+def years_to_minutes(value):
+    return value * 365 * 24 * 60
+
+def minutes_to_years(value):
+    return value / (365 * 24 * 60)
